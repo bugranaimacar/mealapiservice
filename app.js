1d9eed9d7a7a8f099c33560f32a213b997d218f1
Buğra Naim Acar <47912949+bugranaimacar@users.noreply.github.com>
2022-02-23 21:54:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { yemeklisteyukle, zamangetir } = require('./yemekgetir.js');
+const app = express()
+const port = 3000
+
+
+app.set('trust proxy', true);
+
+app.get('/', (req, res) => {
+    var userip = req.headers['cf-connecting-ip']
+  res.status(200).send({
+    ipaddress: userip,
+    application: 'Yemek API Service',
+    routes: '/api/yemekgetir/\:date',
+    usage: '0 -> Shows today\'s meal | 1-> Shows tomorrow\'s meal'
+  })
+})
+
+app.get ('/api/yemekgetir/:date', (req, res) => {
+  res.status(200).send({
+      school: 'Aydın Özel Büyük Uğur Anadolu Lisesi',
+      date: zamangetir(req.params.date),
+      meallist: yemeklisteyukle(req.params.date)
+  });
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
---
+++ yemekgetir.js
@@ -0,0 +1,69 @@
+var XLSX = require('xlsx')
+var workbook = XLSX.readFile('yemek.xlsx');
+var sheet_name_list = workbook.SheetNames;
+var yemeklistejson = JSON.parse(JSON.stringify(workbook.Sheets[sheet_name_list[0]]));
+var yemekliste = JSON.stringify(workbook.Sheets[sheet_name_list[0]]);
+let ts = new Date();
+
+var weekday=new Array(8);
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
+weekday[7]="Sunday";
+
+var months=new Array(12);
+months[0]="January";
+months[1]="February";
+months[2]="March";
+months[3]="April";
+months[4]="May";
+months[5]="June";
+months[6]="July";
+months[7]="August";
+months[8]="September";
+months[9]="October";
+months[10]="November";
+months[11]="December";
+
+let bugununstringi = weekday[parseInt(ts.getDay())] + ", " + months[ts.getMonth()] + " " + ts.getDate() + ", " + ts.getFullYear();
+let yarininstringi = weekday[parseInt(ts.getDay())+1] + ", " + months[ts.getMonth()] + " " + parseInt(ts.getDate()+1) + ", " + ts.getFullYear();
+let yemekanahtar = new Array(8)
+let yemekstringi = "";
+let yazilacakstring;
+
+
+module.exports.zamangetir = function(zamangetir){
+    if(zamangetir == 0) return bugununstringi;
+    else return yarininstringi;
+}
+
+module.exports.yemeklisteyukle = function(zaman){
+    if(zaman == 0) yazilacakstring = bugununstringi;
+    else yazilacakstring = yarininstringi;
+    anahtar = "";
+    yemekstringi = "";
+    for (var key in yemeklistejson) {
+        if (yemeklistejson.hasOwnProperty(key)) {
+           if(yemeklistejson[key].w == yazilacakstring) {
+              //  anahtar = "Key:" + key.charAt(0) + parseInt(key.substring('1')) + " -> " + yemeklistejson[key].w;    
+            for(var i = 0; i < yemekanahtar.length; i++){
+                    let artis = i+1
+                    yemekanahtar[i] = parseInt(key.substring(1))+artis
+                    let mainkey = key.charAt(0) + yemekanahtar[i];
+                    yemekstringi = yemekstringi.concat(yemeklistejson[mainkey]['w'], "\n");
+                }
+           }
+        }
+        else{
+            return "Bu gün için tanımlanmış bir yemek bulunamadı!";
+        }
+    }
+    return yemekstringi || "Bu gün için tanımlanmış bir yemek bulunamadı!";
+}
+
+
+
